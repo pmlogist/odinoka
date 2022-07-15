db62cd5d5ba74a4647b7bb3d32c5aede552925c0
pmlogist <42704376+pmlogist@users.noreply.github.com>
2022-07-15 11:56:50 +0200

    Edit eslint config

--- lua/modules/null-ls.lua
+++ lua/modules/null-ls.lua
@@ -7,13 +7,9 @@ local sources = {
   null_ls.builtins.formatting.nixfmt,
   null_ls.builtins.formatting.stylua,
   null_ls.builtins.formatting.black,
-  null_ls.builtins.code_actions.eslint_d.with {},
-  null_ls.builtins.diagnostics.eslint_d.with {
-    prefer_local = "node_modules/.bin",
-    condition = function(utils)
-      return utils.root_has_file { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
-    end,
-  },
+  -- ESLint
+  null_ls.builtins.code_actions.eslint_d,
+  null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.formatting.prettierd.with {
     prefer_local = "node_modules/.bin",
     -- condition = function(utils)
